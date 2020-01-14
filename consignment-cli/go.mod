dfda15c335942b298b7b6e2c313661db1bece644
mgeri <m.geri1974@gmail.com>
2020-01-14 23:45:11 +0100

    use go micro and docker

--- consignment-cli/go.mod
+++ consignment-cli/go.mod
@@ -3,7 +3,6 @@ module github.com/mgeri/shippy/consignment-cli
 go 1.13
 
 require (
-	github.com/mgeri/shippy/consignment-service v0.0.0-20200113193614-31d046beace4
+	github.com/mgeri/shippy/consignment-service v0.0.0-20200114215535-9d11ea5a394a
 	github.com/micro/go-micro v1.18.0
-	google.golang.org/grpc v1.26.0
 )
--- consignment-cli/main.go
+++ consignment-cli/main.go
@@ -10,7 +10,7 @@ import (
 	"context"
 
 	pb "github.com/mgeri/shippy/consignment-service/proto/consignment"
-	"google.golang.org/grpc"
+	micro "github.com/micro/go-micro"
 )
 
 const (
@@ -29,13 +29,11 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+
+	service := micro.NewService(micro.Name("shippy.consignment.cli"))
+	service.Init()
+
+	client := pb.NewShippingServiceClient("shippy.consignment.service", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
